feb81bae60484fe7ebdeaac63311ef276e612f96
Max Anderson <ma225bj@student.lnu.se>
2020-06-21 16:56:07 +0200

    simple map and enemies traveling along waypoints

---
+++ Assets/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 2f;
+    private Transform target; 
+    private int waypointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoints.waypoints[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (waypointIndex >= Waypoints.waypoints.Length - 1)
+        {
+            Finish();
+            return;
+        }
+        
+        waypointIndex++;
+        target = Waypoints.waypoints[waypointIndex];
+    }
+
+    void Finish()        //when enemy hits finish line
+    {
+        Destroy(gameObject);
+    }
+}
